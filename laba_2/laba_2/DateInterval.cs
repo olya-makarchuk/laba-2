b32cf137b5419335acd177ad1b50aa4988a18606
Makarchuk Olga <olamakarcuk12@gmail.com>
2022-03-26 14:55:29 +0200

    second commit

---
+++ laba_2/laba_2/DateInterval.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_2
+{
+    public class DateInterval
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public int date_res;
+        private int Diff_date()
+        {
+            var res = End - Start;
+            date_res = res.Days;
+
+            return date_res;
+        }
+        public int Print_date
+        {
+            get
+            {
+                return Diff_date();
+            }
+        }
+
+        private int Diff_mon()
+        {
+            var yeardif = End.Year - Start.Year;
+            var mon = yeardif * 12 + (End.Month - Start.Month);
+
+            return mon;
+        }
+        public int Print_mon
+        {
+            get
+            {
+                return Diff_mon();
+            }
+        }
+
+        private int Diff_year()
+        {
+            var yeardiff = End.Year - Start.Year;
+
+            return yeardiff;
+        }
+        public int Print_year
+        {
+            get
+            {
+                return Diff_year();
+            }
+        }
+
+        private string DayOfWeek()
+        {
+            string res = $"\t\t     {Start.DayOfWeek} \t {End.DayOfWeek}";
+            return res;
+        }
+        public string Print_DayWeek
+        {
+            get
+            {
+                return DayOfWeek();
+            }
+        }
+
+        private int Diff_week()
+        {
+            int week = date_res / 7;
+            return week;
+        }
+        public int Print_week
+        {
+            get
+            {
+                return Diff_week();
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Рiзниця мiж датами: {Start}     {End}");
+            Console.WriteLine(Print_DayWeek);
+            Console.WriteLine($"В днях:    {Print_date}");
+            Console.WriteLine($"В тижнях:  {Print_week}");
+            Console.WriteLine($"В мiсяцях: {Print_mon}");
+            Console.WriteLine($"В роках:   {Print_year}");
+        }
+    }
+}
---
+++ laba_2/laba_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace laba_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] date1 = new int[3];
+            int[] date2 = new int[3];
+
+            Console.WriteLine("Перша дата");
+            date1 = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            Console.WriteLine("Друга дата");
+            date2 = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+            DateInterval date = new DateInterval() {Start = new DateTime(date1[2], date1[1], date1[0]), End = new DateTime(date2[2], date2[1], date2[0]) };
+            string path = "date.json";
+
+            string json = JsonSerializer.Serialize(date);
+            File.WriteAllText(path, json);
+            
+            string jsonread =File.ReadAllText(path);
+            DateInterval date_j = JsonSerializer.Deserialize<DateInterval>(jsonread);
+
+            date_j.Print();
+        }
+    }
+}
